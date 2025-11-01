cd1c440810bd088b4bbc0c50301e162a1c9802d3
goldbergVariation <168973433+goldbergVariation@users.noreply.github.com>
2025-11-02 03:13:53 +0900

    Update Dao.java

--- src/main/java/tool/Dao.java
+++ src/main/java/tool/Dao.java
@@ -5,18 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Dao {
+    private static final String URL = System.getenv("DATABASE_URL");
 
     public Connection getConnection() throws SQLException {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("PostgreSQL JDBC Driver not found.", e);
-        }
-
-        String url = System.getenv("DB_URL");
-        String user = System.getenv("DB_USER");
-        String password = System.getenv("DB_PASS");
-
-        return DriverManager.getConnection(url, user, password);
+        return DriverManager.getConnection(URL);
     }
 }
