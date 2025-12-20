696b7cadacfc5ddea39d66e48e26bc1f0d1d9806
goldbergVariation <168973433+goldbergVariation@users.noreply.github.com>
2025-12-20 18:19:34 +0900

    Update Dao.java

--- src/main/java/tool/Dao.java
+++ src/main/java/tool/Dao.java
@@ -1,16 +1,22 @@
-package tool;
+package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
 
 public class Dao {
 
-    public Connection getConnection() throws SQLException ,NamingException {
-        Class.forName("org.postgresql.Driver");
+	public Connection getConnection() throws SQLException, NamingException {
+		try {
+			Class.forName("org.postgresql.Driver");
 
-        String url = System.getenv("DATABASE_URL");  // Render の環境変数
-        return DriverManager.getConnection(url);
-    } catch (NamingException e) {
-		throw new SQLException("データソースの取得に失敗しました", e);
+			String url = System.getenv("DATABASE_URL"); // Render の環境変数
+			return DriverManager.getConnection(url);
+
+		} catch (ClassNotFoundException e) {
+			throw new SQLException("データソースの取得に失敗しました", e);
+		}
 	}
 }
